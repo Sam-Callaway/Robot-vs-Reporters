bbb367a5a8c223e9968ea18719090d5e8980f931
Seb <jonesseb10@gmail.com>
2023-03-16 20:33:41 +0000

    add news api to app

--- src/App.js
+++ src/App.js
@@ -1,64 +1,30 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  // Getters/setters for article header and description
-  const [articleHeader, setArticleHeader] = useState('');
-  const [description, setDescription] = useState('');
+  const [data, setData] = useState(null);
 
-  const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
-
-  // Generate description function
-  const handleGenerateDescription = async () => {
-    try {
-      console.log(OPENAI_KEY);
-      // Create axios post request to chatGPT api
-      const response = await axios.post(
-        'https://api.openai.com/v1/chat/completions',
-        {
-          // Determine api settings
-          "model": "gpt-3.5-turbo",
-          "messages": [{"role": "user", "content": `Generate an article description from from the following header: ${articleHeader}!`}],
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${OPENAI_KEY}`,
-          },
-        }
-      );
-      console.log(response);
-      // Storing required response as variable
-      const generatedText = response.data.choices[0].message.content;
-      // SetDescription method with response from API
-      setDescription(generatedText);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  useEffect(() => {
+    fetch('https://newsdata.io/api/1/news?apikey=pub_189185d557591cb72662c5b99d3dc385d3f91&language=en')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
     <div>
-      <label htmlFor="articleHeader">Enter news article header:</label>
-      {/* Input for the news article */}
-      <input
-        type="text"
-        id="articleHeader"
-        value={articleHeader}
-        onChange={(event) => setArticleHeader(event.target.value)}
-      />
-      <br />
-      <button onClick={handleGenerateDescription}>Generate Description</button>
-      <br />
-      {description && (
-        <>
-          <h2>Generated Description:</h2>
-          <p>{description}</p>
-        </>
+      {data && (
+        <div>
+          <h2 className="title">{data.results[4].title}</h2>
+          <p className="content">{data.results[4].content}</p>
+        </div>
       )}
     </div>
   );
 }
 
 export default App;
-
