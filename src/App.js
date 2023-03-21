3ed249d8fa7dcf9e7c50bd8bc3d4d09ea71b5588
Seb <jonesseb10@gmail.com>
2023-03-21 20:49:34 +0000

    update randomness to ensure that only on page load the elements are swapped

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ function App() {
   const [title, setTitle] = useState(null);
   const [answer, setAnswer] = useState(null);
   const [rightOrWrong, setRightOrWrong] = useState(null);
+  const [isSwapped, setIsSwapped] = useState(Math.random() < 0.5);
 
   useEffect(() => {
-    fetch('https://newsdata.io/api/1/news?apikey=pub_189185d557591cb72662c5b99d3dc385d3f91&language=en')
+    fetch('https://newsdata.io/api/1/news?apikey=pub_19285cce4ce7dea610f3df1a8b4a1ef875aa1&language=en')
       .then(response => response.json())
       .then(data => {
         console.log(data);
@@ -25,7 +26,6 @@ function App() {
         console.error('Error fetching data:', error);
       });
   }, []);
-
 
   function checkAnswer(event) {
     const choice = event.target.id;
@@ -43,19 +43,33 @@ function App() {
         <div>
           <h2 className="title">{data.results[4].title}</h2>
           <Row>
-            <Col>
-              <GenerateDesc title={title} />
-              <Button onClick={checkAnswer} id="chatGPT">This is the real description!</Button>
-            </Col>
-            <Col>
-              <h2>Journalist Description: </h2>
-              <p className="content">{content}</p>
-              <Button onClick={checkAnswer} id="journalist">No! This is the real description!</Button>
-            </Col>
+            {isSwapped ? (
+              <>
+                <Col>
+                  <h2>Journalist Description: </h2>
+                  <p className="content">{content}</p>
+                  <Button onClick={checkAnswer} id="journalist">No! This is the real description!</Button>
+                </Col>
+                <Col>
+                  <GenerateDesc title={title} />
+                  <Button onClick={checkAnswer} id="chatGPT">This is the real description!</Button>
+                </Col>
+              </>
+            ) : (
+              <>
+                <Col>
+                  <GenerateDesc title={title} />
+                  <Button onClick={checkAnswer} id="chatGPT">This is the real description!</Button>
+                </Col>
+                <Col>
+                  <h2>Journalist Description: </h2>
+                  <p className="content">{content}</p>
+                  <Button onClick={checkAnswer} id="journalist">No! This is the real description!</Button>
+                </Col>
+              </>
+            )}
           </Row>
-
           <h2>{rightOrWrong}</h2>
-
         </div>
       )}
     </div>
