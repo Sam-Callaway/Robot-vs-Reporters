1e77ab1c2bc50f56f8035329f6e7439b8ae1f860
ra724619@gmail.com <raymond131415@Tses-MacBook-Pro.local>
2023-03-21 18:49:02 +0000

    Added Dark Mode Button and App Bar by MUI

--- public/env.js
+++ public/env.js
@@ -59,6 +59,5 @@ window.env = {
   "SECURITYSESSIONID": "186ac",
   "npm_node_execpath": "/opt/homebrew/Cellar/node/19.8.0/bin/node",
   "npm_config_prefix": "/opt/homebrew/Cellar/node/19.8.0",
-  "COLORTERM": "truecolor",
-  "REACT_APP_OPENAI_KEY": "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"
+  "COLORTERM": "truecolor"
 };
