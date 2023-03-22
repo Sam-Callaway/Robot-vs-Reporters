4997767d0332c340149d3d3a9d11946cce40d19f
Seb <jonesseb10@gmail.com>
2023-03-22 23:04:46 +0000

    fix the swapping. the bug is fixed git add -A

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import DarkModeSwitch from '../darkmode';
 import Toolbar from '@mui/material/Toolbar';
 import { Typography } from '@mui/material';
 
-function LoadingScreen (){
 
-    const loadingTitles = [];
+function LoadingScreen() {
+
+    const [loadingMsg, setLoadingMsg] = useState(null);
+
+    const loadingTitles = ["Man vs Machine - the long debated topic is unfolding in front of us.", "Will we all be replaced by AI? (probably not while it takes this long to get a response)", "Is creativity dead?", "Should we all have listened to Elon Musk years ago?"];
+
+    useEffect(() => {
+        setLoadingMsg(loadingTitles[Math.floor(Math.random() * 4)]);
+      }, []); 
 
 
-
-return(
-    <div>
-        <h1></h1>
-    </div>
+    return (
+        <div className="d-flex justify-content-center align-items-center" style={{height: "110vh"}}>
+            <h4 >{loadingMsg}</h4>
+        </div>
     )
 }
 export default LoadingScreen
--- src/components/RvsR/RvsR.js
+++ src/components/RvsR/RvsR.js
@@ -3,18 +3,19 @@ import GenerateDesc from "../../utils/generateDesc";
 import newsscraper from '../../utils/newsscraper';
 import TopAppBar from '../TopBar/appbar';
 import env from 'react-dotenv';
-import { Grid,Box,Button,Toolbar,Typography,Switch,Paper } from '@mui/material';
+import { Grid, Box, Button, Toolbar, Typography, Switch, Paper, Card, CardContent } from '@mui/material';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import LoadingScreen from "../LoadingScreen/LoadingScreen.js";
 // Grid item styling
 
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+// // Grid item styling
+// const Item = styled(Paper)(({ theme }) => ({
+//     ...theme.typography.body2,
+//     padding: theme.spacing(1),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+//   }));
 
 
 function RvsR(props) {
@@ -26,7 +27,7 @@ function RvsR(props) {
     const [answer, setAnswer] = useState(null);
     const [rightOrWrong, setRightOrWrong] = useState(null);
     // Swapping description elements randomly
-    const [isSwapped, setIsSwapped] = useState(Math.random() < 0.5);
+    const [isSwapped, setIsSwapped] = useState(false);
     // Generate random number for indice of title element
     const [number, setNumber] = useState(null);
     // Tracking scores
@@ -47,59 +48,60 @@ function RvsR(props) {
     // Creating Dark Mode theme
     const lightTheme = createTheme({
         palette: {
-          mode: 'light',
-          primary: {
-            main: '#6A1B9A',
-          },
-          secondary: {
-            main: '#4CAF50',
-          },
-          background: {
-            default: '#fbf3e5',
-          },
-          text: {
-            primary: '#F76F72',
-            secondary: '#4CAF50',
-          },
+            mode: 'light',
+            primary: {
+                main: '#6A1B9A',
+            },
+            secondary: {
+                main: '#4CAF50',
+            },
+            background: {
+                default: '#D9D9D9',
+            },
+            text: {
+                primary: '#F76F72',
+                secondary: '#4CAF50',
+            },
         },
-      });
-    
-      const darkTheme = createTheme({
+    });
+
+    const darkTheme = createTheme({
         palette: {
-          mode: 'dark',
-          primary: {
-            main: '#007AFF',
-          },
-          secondary: {
-            main: '#FF3B30',
-          },
-          background: {
-            default: '#000000',
-          },
-          text: {
-            primary: '#13ED65',
-            secondary: '#4CAF50',
-          },
-          button: {
-            default: '#000000',
-          },
+            mode: 'dark',
+            primary: {
+                main: '#007AFF',
+            },
+            secondary: {
+                main: '#FF3B30',
+            },
+            background: {
+                default: '#000000',
+            },
+            text: {
+                primary: '#13ED65',
+                secondary: '#4CAF50',
+            },
+            button: {
+                default: '#000000',
+            },
         },
-      });
-    
-      const theme = isDarkMode ? darkTheme : lightTheme;
-    
-      const handleChange = () => {
+    });
+
+
+    const theme = isDarkMode ? darkTheme : lightTheme;
+
+    const handleChange = () => {
         setIsDarkMode(!isDarkMode);
-      };  
+    };
 
     useEffect(() => {
-        fetch('https://newsdata.io/api/1/news?apikey=pub_19332709e568981e985f21187a879f94062f3&language=en')
+        fetch('https://newsdata.io/api/1/news?apikey=pub_19337c0d21c654df0435eff36fe6a5f093d8e&language=en')
             .then(response => response.json())
             .then(data => {
                 console.log(data);
                 setData(data);
-                setContent(data.results[7].content.split(' ').slice(0, 80).join(' '));
-                setTitle(data.results[7].title);
+                setContent(data.results[0].content.split(' ').slice(0, 80).join(' '));
+                setTitle(data.results[0].title);
                 setAnswer(answer);
             })
             .catch(error => {
@@ -121,120 +123,138 @@ function RvsR(props) {
 
         setNextRound("Load next title");
         handleGenerateNumber();
-       
+
     }
 
     const handleGenerateNumber = () => {
-      console.log(number)
+        console.log(number)
         const randomNum = Math.floor(Math.random() * 9);
-        if (randomNum === number){             
-            if((randomNum+1) === 10)
-                {setNumber(0)}else{setNumber(randomNum+1)}            
-            }else{
-              setNumber(randomNum);}
+        if (randomNum === number) {
+            if ((randomNum + 1) === 10) { setNumber(0) } else { setNumber(randomNum + 1) }
+        } else {
+            setNumber(randomNum);
+        }
 
     };
 
     function handleNextRound() {
-        
-            setContent(data.results[number].content.split(' ').slice(0, 80).join(' '));
-            setTitle(data.results[number].title);
-            setGptLoaded(false);
-            return (
-                <RvsR />
-            )
+        setIsSwapped(Math.random() < 0.5);
+        setContent(data.results[number].content.split(' ').slice(0, 80).join(' '));
+        setTitle(data.results[number].title);
+        setGptLoaded(false);
+        return (
+            <RvsR />
+        )
     }
 
     let showRvsR = {}
     let showLoading = {}
-    if(gptLoaded === false){
+    if (gptLoaded === false) {
         showRvsR = {
-           display: 'none'
+            display: 'none'
         };
-    } else{showRvsR = {};}
-    
-    if(gptLoaded === false){
+    } else { showRvsR = {}; }
+
+    if (gptLoaded === false) {
         showLoading = {};
-    } else{showLoading = {
-        display: 'none'}}
+    } else {
+        showLoading = {
+            display: 'none'
+        }
+    }
 
 
 
     return (
         <div>
-    <ThemeProvider theme={theme}>
-    <CssBaseline enableColorScheme />
-        <TopAppBar/>
-        <div style={showLoading}>
-            <LoadingScreen />
-            </div>
-            {data && (
-                <div style={showRvsR}>
-                    <h2 className="title d-flex justify-content-center align-items-center">Article Title : {title}</h2>
-                    <Box sx={{ flexGrow:1}}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline enableColorScheme />
+                <Toolbar color='transparent' position="static" >
+                    <Typography variant="h4" component="div" sx={{ flexGrow: 1 }} position="center" align="center">
+                        Robot VS Reporters
+                    </Typography>
+                    <Typography>
+                        <Switch
+                            checked={isDarkMode}
+                            onChange={handleChange}
+                            align="right"
+                        />
 
-                    {isSwapped ? (
-                    <Grid 
-                    container
-                    justifyContent="center"
-                    alignItems="stretch"
-                    colums={12}
-                    > 
-
-                    <Grid xs={6}>
-                    <Item>
-                    <h2> Description: </h2>
-                    <p className="content">{content}</p>
-                    <Button variant="outlined" onClick={checkAnswer} id="journalist"> This is the real description!</Button>
-                    </Item>
-                    </Grid>
-                    <Grid xs={6}>
-                    <Item>
-                    <GenerateDesc gptIsLoaded = {gptIsLoaded} title={title} />
-                    <Button variant="outlined" onClick={checkAnswer} id="chatGPT">This is the real description!</Button>
-                    </Item>
-                    </Grid>
-                    </Grid> 
-                    ) : (
-                    <Grid 
-                    container
-                    justifyContent="center"
-                    alignItems="stretch"
-                    colums={12}
-                    > 
-                    <Grid xs={6}>
-                    <Item>
-                    <GenerateDesc gptIsLoaded = {gptIsLoaded} title={title} />
-                    <Button onClick={checkAnswer} variant="outlined" id="chatGPT">This is the real description!</Button>
-                    </Item>
-                    </Grid>
-                    <Grid xs={6}>
-                    <Item>
-                    <h2> Description: </h2>
-                    <p className="content">{content}</p>
-                    <Button onClick={checkAnswer} variant="outlined" id="journalist"> This is the real description!</Button>
-                    </Item>  
-                    </Grid>  
-                    </Grid>
-
-                        )}
-                    <Grid
-                    container
-                    justifyContent="center"
-                    colums={12}
-                    >
-                    <Grid xs={12}>
-                    <Item>
-                    <h2 className='d-flex align-items-center justify-content-center'>{rightOrWrong}</h2>
-                    <h2 className='d-flex align-items-center justify-content-center'>Robots: {robotScore} vs  Reporters: {reportScore}</h2>
-                    <Button variant="outlined" onClick={handleNextRound}>{nextRound}</Button>
-                    </Item>
-                    </Grid>
-                    </Grid>
-                    </Box>
+                    </Typography>
+                </Toolbar>
+                <div style={showLoading}>
+                    <LoadingScreen title={title} />
                 </div>
-            
-            )}
+                {data && (
+                    <div style={showRvsR}>
+                        <h2 className="title">{title}</h2>
+                        <Box sx={{ flexGrow: 1 }}>
+
+                            {isSwapped ? (
+                                <>
+                                    <Grid
+                                        container
+                                        justifyContent="center"
+                                        alignItems="stretch"
+                                        colums={12}
+                                        spacing={4}
+                                    >
+                                        <Grid item xs={6}>
+                                            <Card variant="outlined" sx={{ height: '400px' }}>
+                                                <h2> Description: </h2>
+                                                <p className="content">{content}</p>
+                                                <Button variant="outlined" onClick={checkAnswer} id="journalist"> This is the real description!</Button>
+                                            </Card>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Card variant="outlined" sx={{ height: '400px' }}>
+                                                <GenerateDesc gptIsLoaded={gptIsLoaded} title={title} />
+                                                <Button variant="outlined" onClick={checkAnswer} id="chatGPT">This is the real description!</Button>
+                                            </Card>
+                                        </Grid>
+                                    </Grid>
+                                </>
+                            ) : (
+                                <>
+                               <Grid
+                                    container
+                                    justifyContent="center"
+                                    alignItems="stretch"
+                                    spacing={2}
+                                >
+                                    <Grid item xs={6}>
+                                        <Card variant="outlined" sx={{ height: '400px' }}>
+                                            <GenerateDesc gptIsLoaded={gptIsLoaded} title={title} />
+                                            <Button onClick={checkAnswer} variant="outlined" id="chatGPT">This is the real description!</Button>
+                                        </Card>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Card variant="outlined" sx={{ height: '400px' }}>
+                                            <h2> Description: </h2>
+                                            <p className="content">{content}</p>
+                                            <Button onClick={checkAnswer} variant="outlined" id="journalist"> This is the real description!</Button>
+                                        </Card>
+                                    </Grid>
+                                </Grid>
+                            </>)}
+                            <Grid
+                                container
+                                justifyContent="center"
+                                colums={12}
+                                sx={{ paddingTop: "20px" }}
+                            >
+                                <Grid item xs={12}>
+                                    <Card variant="outlined">
+                                        <h2 className='d-flex align-items-center justify-content-center'>{rightOrWrong}</h2>
+                                        <h2 className='d-flex align-items-center justify-content-center'>Robots: {robotScore} vs  Reporters: {reportScore}</h2>
+                                        <Button variant="outlined" onClick={handleNextRound}>{nextRound}</Button>
+                                    </Card>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </div>
+
+                )}
             </ThemeProvider>
         </div>
     );
