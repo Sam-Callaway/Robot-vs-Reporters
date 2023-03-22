134298319440d89df91fe3e2fd817f709b0119fe
Sam Callaway <calls002@gmail.com>
2023-03-22 23:24:36 +0000

    fixes

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -1,24 +1,33 @@
-import React, { useState, useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import DarkModeSwitch from '../darkmode';
 import Toolbar from '@mui/material/Toolbar';
 import { Typography } from '@mui/material';
 
-
-function LoadingScreen() {
-
-    const [loadingMsg, setLoadingMsg] = useState(null);
-
-    const loadingTitles = ["Man vs Machine - the long debated topic is unfolding in front of us.", "Will we all be replaced by AI? (probably not while it takes this long to get a response)", "Is creativity dead?", "Should we all have listened to Elon Musk years ago?"];
-
+function LoadingScreen (props){
+    
+    const [message, setMessage] = useState("")
+    const loadingScreenMessages = [
+        "Man vs Machine - the long debated topic is unfolding in front of us.",
+        "Will we all be replaced by AI? (probably not while it takes this long to get a response)",
+        "Is creativity dead?",
+        "ChatGPT wrote most of these",
+        "The proliferation of fake news underscores the need for responsible AI development.",
+        "AI-generated fake news: because the internet wasn't already confusing enough.",
+        "When it comes to AI taking over jobs, let's just hope they don't replace comedians.",
+        "How do we ensure that everyone benefits from automation?",
+        "Robots taking over jobs - it's like a game of musical chairs, but instead of chairs, it's careers."
+    ]
+    const newMessage = () => {
+        let nextMessage = loadingScreenMessages[(Math.floor(Math.random() * 9) + 0)]
+        console.log(nextMessage)
+        setMessage(nextMessage)}
     useEffect(() => {
-        setLoadingMsg(loadingTitles[Math.floor(Math.random() * 4)]);
-      }, []); 
-
-
-    return (
-        <div className="d-flex justify-content-center align-items-center" style={{height: "110vh"}}>
-            <h4 >{loadingMsg}</h4>
-        </div>
+        newMessage();
+      },[props.title]);
+return(
+    <div className="d-flex justify-content-center align-items-center" style={{height: "110vh"}}>
+    <h4 >{message}</h4>
+    </div>
     )
 }
 export default LoadingScreen
